a5b314e8e4a4b54fabc69573d9451cb80f5eab6b
Rreuben <jreuben07@gmail.com>
2018-06-18 14:55:35 +0300

    Add child component called landing-page

---
+++ src/app/github-landing-page/github-landing-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github-landing-page',
+  templateUrl: './github-landing-page.component.html',
+  styleUrls: ['./github-landing-page.component.css']
+})
+export class GithubLandingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
