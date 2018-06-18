419db502d6118a181d89a9928cd4ec1e2653786a
Rreuben <jreuben07@gmail.com>
2018-06-18 23:05:45 +0300

    Fix errors and add a getRepo function

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -10,11 +10,16 @@ import { ProfileService } from '../services/profile.service' ;
 export class GithubProfileComponent implements OnInit {
 
   profile : any[];
+  repos : any[];
 
   constructor( private profileService : ProfileService ) {
     this.profileService.getProfileInfo().subscribe( profile => {
       console.log( profile ) ;
       this.profile = profile ;
+    });
+    this.profileService.getProfileRepos().subscribe( repos => {
+      console.log( repos ) ;
+      this.repos = repos ;
     });
    }
 
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -22,4 +22,8 @@ export class ProfileService {
     .pipe( map( res => res.json() ) );
   }
 
+  getProfileRepos() {
+    return this.http.get( 'https://api.github.com/users/' + this.username + '/repos?client_id=' + this.clientid + '&client_secret=' + this.clientsecret)
+    .pipe( map( res => res.json() ) );
+  }
 }
