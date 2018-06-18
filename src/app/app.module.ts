414632c264a0462fd65a6e37171bb745750a5229
Rreuben <jreuben07@gmail.com>
2018-06-18 14:52:34 +0300

    Create default angular web app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
 import { GithubFormComponent } from './github-form/github-form.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { GithubLandingPageComponent } from './github-landing-page/github-landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubComponent,
-    GithubFormComponent
+    GithubFormComponent,
+    GithubProfileComponent,
+    GithubLandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
